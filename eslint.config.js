15a515660bedf3885080876a5ad32442d28a7854
chen4023 <ads4023@naver.com>
2025-11-18 21:33:30 +0900

    fix: easy 충돌 해결

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,12 @@ import eslintConfigPrettier from "eslint-config-prettier";
 import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
 
 /** @type {import('eslint').Linter.Config[]} */
+<<<<<<< HEAD
 export default [
+=======
+export default tseslint.config([
+  { ignores: ["**/*/dist", "**/*/node_modules", "build"] },
+>>>>>>> bef0aef (feat: eslint 설정 추가)
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   eslintPluginPrettier,
